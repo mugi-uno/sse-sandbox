d9829f93d2365e1d731cd9e3d9f6004d0c1e8713
mugi-uno <mugi.uno@gmail.com>
2024-06-16 11:19:29 +0900

    move

---
+++ cloudflare/src/index.ts
@@ -0,0 +1,12 @@
+const TEXT =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'.repeat(
+    1000
+  );
+
+export default {
+  async fetch(request, env, ctx): Promise<Response> {
+    const reader = new Blob([TEXT]).stream();
+
+    return new Response(reader, { headers: { 'Content-Type': 'text/event-stream' } });
+  },
+} satisfies ExportedHandler<Env>;
