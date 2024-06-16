45655f8e8b87f06c740c0b33199b5c706e76645d
mugi-uno <mugi.uno@gmail.com>
2024-06-16 11:37:10 +0900

    sse api

--- cloudflare/src/index.ts
+++ cloudflare/src/index.ts
@@ -1,12 +1,22 @@
 const TEXT =
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'.repeat(
-    1000
-  );
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
 export default {
   async fetch(request, env, ctx): Promise<Response> {
-    const reader = new Blob([TEXT]).stream();
+    const { readable, writable } = new TransformStream();
+    const writer = writable.getWriter();
+    const encoder = new TextEncoder();
+    let chunks = TEXT.split(' ');
 
-    return new Response(reader, { headers: { 'Content-Type': 'text/event-stream' } });
+    const intervalId = setInterval(() => {
+      const chunk = chunks.shift();
+      writer.write(encoder.encode(`data: ${chunk}\n\n`));
+
+      if (chunks.length === 0) {
+        clearInterval(intervalId);
+      }
+    }, 100);
+
+    return new Response(readable, { headers: { 'Content-Type': 'text/event-stream' } });
   },
 } satisfies ExportedHandler<Env>;
