78bd0204608f8b58ee655d090f410decd9a744a8
mugi-uno <mugi.uno@gmail.com>
2024-06-16 20:43:38 +0900

    update

--- client/src/app/eventsource/page.tsx
+++ client/src/app/eventsource/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
       <View url="https://sse-sandbox.mugi-uno.workers.dev/" />
       <hr />
       <h3>Deno Deploy SSE & EventSource</h3>
-      <View url="https://mugi-uno-sse-sandbox.deno.dev/api/sse/" />
+      <View url="https://mugi-uno-sse-sandbox.deno.dev/api/sse" />
       <hr />
     </main>
   );
