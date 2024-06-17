1569d03b8124d048478ab6f00ce4ea4e9223f722
mugi-uno <mugi.uno@gmail.com>
2024-06-17 21:31:25 +0900

    update hono

--- cloudflare-hono/src/index.ts
+++ cloudflare-hono/src/index.ts
@@ -1,6 +1,6 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
-import { streamText } from "hono/streaming";
+import { streamSSE, streamText } from "hono/streaming";
 
 const TEXT =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
@@ -10,36 +10,26 @@ const app = new Hono();
 app.use("/*", cors());
 
 app.get("/sse", (c) => {
-  return streamText(c, async (stream) => {
-    c.header("Content-Type", "text/event-stream");
-
+  return streamSSE(c, async (stream) => {
     let chunks = TEXT.split(" ");
 
     while (chunks.length > 0) {
-      const chunk = chunks.shift();
+      const chunk = chunks.shift()!;
       await stream.sleep(100);
-      await stream.writeln(`data: ${chunk}`);
-      await stream.writeln("");
+      await stream.writeSSE({ data: chunk });
     }
   });
 });
 
 app.post("/sse-post", (c) => {
-  return streamText(c, async (stream) => {
-    c.header("Content-Type", "text/event-stream");
-
+  return streamSSE(c, async (stream) => {
     let chunks = TEXT.split(" ");
 
     while (chunks.length > 0) {
-      const chunk = chunks.shift();
+      const chunk = chunks.shift()!;
       await stream.sleep(100);
-      await stream.writeln(`data: ${chunk}`);
-      await stream.writeln("");
+      await stream.writeSSE({ data: chunk });
     }
-
-    await stream.writeln(`event: close`);
-    await stream.writeln(`data:`);
-    await stream.writeln("");
   });
 });
 
