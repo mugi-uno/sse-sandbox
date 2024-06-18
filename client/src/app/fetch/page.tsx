19334a151b371e31f1e52bd34300566bb03d4f43
mugi-uno <mugi.uno@gmail.com>
2024-06-18 13:04:36 +0900

    update

--- client/src/app/fetch/page.tsx
+++ client/src/app/fetch/page.tsx
@@ -3,22 +3,22 @@ import { View } from "./View";
 export default function Page() {
   return (
     <main>
-      <h3>Cloudflare Workers SSE & EventSource</h3>
+      <h3>Cloudflare Workers SSE & fetch()</h3>
       <View url="https://sse-sandbox.mugi-uno.workers.dev/" />
 
       <hr />
 
-      <h3>Cloudflare Workers SSE (Hono) & EventSource</h3>
+      <h3>Cloudflare Workers SSE (Hono) & fetch()</h3>
       <View url="https://sse-sandbox-hono.mugi-uno.workers.dev/sse" />
 
       <hr />
 
-      <h3>Cloudflare Workers SSE (Hono) & EventSource & POST</h3>
+      <h3>Cloudflare Workers SSE (Hono) & fetch() & POST</h3>
       <View url="https://sse-sandbox-hono.mugi-uno.workers.dev/sse-post" />
 
       <hr />
 
-      <h3>Deno Deploy SSE & EventSource</h3>
+      <h3>Deno Deploy SSE & fetch()</h3>
       <View url="https://mugi-uno-sse-sandbox.deno.dev/api/sse" />
       <hr />
     </main>
